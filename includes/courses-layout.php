bc8ed6b45b6e800c9c1fd08077f0b9e618e3456b
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-04-19 09:24:49 -0400

    added bootstrap

--- includes/courses-layout.php
+++ includes/courses-layout.php
@@ -23,46 +23,55 @@ function courses_form_display() {
 							<?php endfor; ?>
 						</select>
 					</div>
-					<select name="instructor">
-						<option value=-1>All</option>
-						<?php for ( $i = 0; $i < count( $instructor_arr ); $i++ ) : ?>
-							<option value="<?= $instructor_arr[ $i ]->PeopleID ?>"><?= $instructor_arr[ $i ]->LastFirstName ?></option>
-						<?php endfor; ?>
-					</select>
-					<select name="course">
-						<option value=0>All</option>
-						<?php for ( $i = 0; $i < count( $course_arr ); $i++ ) : ?>
-							<option value="<?= $course_arr[ $i ]->CourseID ?>"><?= $course_arr[ $i ]->FullCourseName ?></option>
-						<?php endfor; ?>
-					</select>
-					<select id="level" name="level">
-						<option value=2>All</option>
-						<option value=1>Undergraduate</option>
-						<option value=0>Graduate</option>
-					</select>
-					<input name="submit" type="submit">
+					<div class="form-group">
+						<label for="instructor">Instructor</label>
+						<select name="instructor" id="instructor" class="form-control">
+							<option value=-1>All</option>
+							<?php for ( $i = 0; $i < count( $instructor_arr ); $i++ ) : ?>
+								<option value="<?= $instructor_arr[ $i ]->PeopleID ?>"><?= $instructor_arr[ $i ]->LastFirstName ?></option>
+							<?php endfor; ?>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="course">Course</label>
+						<select name="course" id="course" class="form-control">
+							<option value=0>All</option>
+							<?php for ( $i = 0; $i < count( $course_arr ); $i++ ) : ?>
+								<option value="<?= $course_arr[ $i ]->CourseID ?>"><?= $course_arr[ $i ]->FullCourseName ?></option>
+							<?php endfor; ?>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="level">Level</label>
+						<select id="level" name="level" class="form-control">
+							<option value=2>All</option>
+							<option value=1>Undergraduate</option>
+							<option value=0>Graduate</option>
+						</select>
+					</div>
+					<button type="submit" name="submit" class="btn btn-primary">Submit</button>
 				</form>
 			</div>
-		</div>
-	</div>
-	<div class="col">
-		<?php
-		if ( isset( $_POST['semester'] ) && isset( $_POST['instructor'] ) && isset( $_POST['course'] ) && isset( $_POST['level'] ) ) {
-			$semester = $_POST['semester'];
-			$instructor = $_POST['instructor'];
-			$course = $_POST['course'];
-			$level = $_POST['level'];
-			if ( $semester == 0 && $instructor == -1 && $course == 0 ) {
-				echo 'Select a semester, instructor, or course';
-			} else {
-				if ( has_filter( 'courses_display' ) ) {
-					echo apply_filters( 'courses_display', $semester, $instructor, $course, $level );
+			<div class="col">
+				<?php
+				if ( isset( $_POST['semester'] ) && isset( $_POST['instructor'] ) && isset( $_POST['course'] ) && isset( $_POST['level'] ) ) {
+					$semester = $_POST['semester'];
+					$instructor = $_POST['instructor'];
+					$course = $_POST['course'];
+					$level = $_POST['level'];
+					if ( $semester == 0 && $instructor == -1 && $course == 0 ) {
+						echo 'Select a semester, instructor, or course';
+					} else {
+						if ( has_filter( 'courses_display' ) ) {
+							echo apply_filters( 'courses_display', $semester, $instructor, $course, $level );
+						}
+					}
+				} else {
+					echo apply_filters( 'courses_display', semester_serial(), -1, 0, 2 );
 				}
-			}
-		} else {
-			echo apply_filters( 'courses_display', semester_serial(), -1, 0, 2 );
-		}
-		?>
+				?>
+			</div>
+		</div>
 	</div>
 	<?php
 	return ob_get_clean();
