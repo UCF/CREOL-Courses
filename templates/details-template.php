667ae53c5e3082af8ac146a9d8e30bd3b0c8c442
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-04-19 12:44:41 -0400

    test

--- templates/details-template.php
+++ templates/details-template.php
@@ -9,9 +9,11 @@ function details_page( $course_id ) {
 <div class="container">
 	<h1 class="mt-3 mt-sm-4 mt-md-5 mb-3"><?= $details_arr[0]->CourseName ?></h1>
 	<?= $details_arr[0]->Description ?>
-	<br><br>
-	<?= $details_arr[0]->Details ?>
+	<div class="mt-4 mb-5 pb-sm-4">
+		<?= $details_arr[0]->Details ?>
+	</div>
 </div>
+
 <?php
 }
 
