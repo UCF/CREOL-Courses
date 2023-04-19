4ada75fb9eef7fc39a5cdf7596e71fb9a6a248bd
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-04-19 12:29:54 -0400

    test

--- templates/details-template.php
+++ templates/details-template.php
@@ -2,14 +2,15 @@
 /*
 Template Name: Course Details Template
 */
+get_header();
+the_post(); 
+
 $url = 'https://api.creol.ucf.edu/CoursesJson.asmx/Details?CourseID=' . $course_id;
 $details_arr = get_json( $url );
 if ( isset( $_GET['courseid'] ) ) {
 	$post->post_title = $details_arr[0]->CourseName;
 }
-
-get_header();
-the_post(); ?>
+?>
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
 	<?php
