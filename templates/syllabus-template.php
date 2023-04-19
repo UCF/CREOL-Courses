a2c2ef532c5beb6bc8421665fa19f255ba75237d
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-04-19 08:44:26 -0400

    test

--- templates/syllabus-template.php
+++ templates/syllabus-template.php
@@ -4,14 +4,14 @@ Template Name: Course Syllabus Template
 */
 function syllabus_pdf( $schedule_id, $course ) {
 	$url = 'https://api.creol.ucf.edu/CoursesJson.asmx/Syllabus?CourseScheduleID=' . $schedule_id;
-	$content = wp_remote_get( $url, array( 'sslverify' => false ) );
+	$content = wp_remote_get( $url );
 	if ( is_wp_error( $content ) ) {
 		echo 'Error';
 		return false;
 	}
 
-	header( 'Content-Type: application/pdf' );
-	header( 'Content-disposition: attachment; filename=' . $course . '-syllabus.pdf' );
+	// header( 'Content-Type: application/pdf' );
+	// header( 'Content-disposition: attachment; filename=' . $course . '-syllabus.pdf' );
 	echo wp_remote_retrieve_body( $content );
 }
 
