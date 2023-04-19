b322931d3dbb9745cb2152309fe062c33a87a87e
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-04-19 11:22:27 -0400

    test

--- includes/courses-layout.php
+++ includes/courses-layout.php
@@ -73,6 +73,7 @@ function courses_form_display() {
 					}
 				} else {
 					$_POST['semester'] = semester_serial();
+					echo $_POST['semester'];
 					echo apply_filters( 'courses_display', semester_serial(), -1, 0, 2 );
 				}
 				?>
