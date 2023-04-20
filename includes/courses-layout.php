6a544cde4c63e0a929597175f18e202f437d93bb
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-04-20 08:44:19 -0400

    testing checkboxes

--- includes/courses-layout.php
+++ includes/courses-layout.php
@@ -61,14 +61,14 @@ function courses_form_display() {
 					<div class="form-check">
 						<label class="form-check-label">
 							<input class="form-check-input" type="checkbox" name="undergrad" value=1 
-							<?= ( isset($_POST['level']) && ( $_POST['level'] == 2 || $_POST['level'] == 1 ) ) ? 'checked=true' : '' ?>>
+							<?= ( isset($_POST['level']) && ( $_POST['level'] == 2 || $_POST['level'] == 1 ) ) ? 'checked' : '' ?>>
 							 Undergraduate
 						</label>
 					</div>
 					<div class="form-check">
 						<label class="form-check-label">
 							<input class="form-check-input" type="checkbox" name="grad" value=0
-							<?= ( isset($_POST['level']) && ( $_POST['level'] == 2 || $_POST['level'] == 0 ) ) ? 'checked=true' : '' ?>>
+							<?= ( isset($_POST['level']) && ( $_POST['level'] == 2 || $_POST['level'] == 0 ) ) ? 'checked' : '' ?>>
 							 Graduate
 						</label>
 					</div>
