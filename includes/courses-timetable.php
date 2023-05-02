d8f23028fb0c5be22040a6bf29a7e5d3bfbe3c20
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-02 11:51:53 -0400

    added timetable

---
+++ includes/courses-timetable.php
@@ -0,0 +1,85 @@
+<?php
+
+function timetable_form_display() {
+    $semester_arr = get_json( 'https://api.creol.ucf.edu/CoursesJson.asmx/SemesterList' );
+
+    ob_start();
+    ?>
+    <div class="container">
+        <div class="row">
+            <form method="post" name="form">
+                <div class="col-3 form-group">
+                    <label for="semester">Semester</label>
+					<select name="semester" id="semester" class="form-control">
+						<option value=0>All</option>
+						<?php for ( $i = 0; $i < count( $semester_arr ); $i++ ) : ?>
+							<option value="<?= $semester_arr[ $i ]->SemesterSerial ?>" 
+							<?= ( isset($_POST['semester']) && $_POST['semester'] == $semester_arr[ $i ]->SemesterSerial ) ? 'selected=true' : '' ?>>
+								<?= $semester_arr[ $i ]->SemesterTxt ?>
+							</option>
+						<?php endfor; ?>
+					</select>
+                </div>
+                <div class="col-2">
+                    <div class="form-check">
+						<label class="form-check-label">
+							<input id="undergrad" class="form-check-input" type="checkbox" name="undergrad" value=1 
+							<?= ( isset($_POST['undergrad'] ) &&  $_POST['undergrad'] == 1 ) ? 'checked' : '' ?>>
+							 Undergraduate
+						</label>
+					</div>
+					<div class="form-check">
+						<label class="form-check-label">
+							<input id="grad" class="form-check-input" type="checkbox" name="grad" value=0
+							<?= ( isset($_POST['grad'] ) && $_POST['grad'] == 0 ) ? 'checked' : '' ?>>
+							 Graduate
+						</label>
+					</div>
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <div class="col-1">
+			    <hr class="hr-vertical h-75">
+		    </div>
+            <div class="col">
+                <p class="d-inline-flex">
+                    <span class="p-2" style="background-color: #99CCFF;">102</span><br>
+                    <span class="p-2" style="background-color: #66CCFF;">Online</span>
+                </p>
+                <span class="p-2" style="background-color: #FFFF99;">103</span>
+                <span class="p-2" style="background-color: #C0C0C0;">265</span>
+                <span class="p-2" style="background-color: #FF9966;">266</span>
+                <span class="p-2" style="background-color: #CC6699;">A207</span>
+                <span class="p-2" style="background-color: #FF99FF;">A210</span>
+                <p class="d-inline-flex">
+                    <span class="p-2" style="background-color: #D0FFD0;">A214</span>
+                    <span class="p-2" style="background-color: #99DD99;">Online</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <?php
+        if ( isset( $_POST['semester'] ) && ( isset( $_POST['undergrad'] ) || isset( $_POST['grad'] ) ) ) {
+            echo $_POST['semester'] . ' ' . $_POST['undergrad'] . ' ' . $_POST['grad'];
+        } else {
+        ?>
+            <script>
+			    // Sets the form to the correct information.
+				document.getElementById("semester").selectedIndex = 1;
+				document.getElementById("instructor").selectedIndex = 0;
+			    document.getElementById("course").selectedIndex = 0;
+				document.getElementById("undergrad").checked = true;
+				document.getElementById("grad").checked = true;
+			</script>
+        <?php
+        }
+        ?>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+
+function timetable_display( $semester, $level ) {
+    
+}
