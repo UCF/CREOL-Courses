fe7239785eb43fdce115d1f2e3f1563aa4ccbb04
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-02 14:03:21 -0400

    test

--- includes/courses-timetable.php
+++ includes/courses-timetable.php
@@ -34,6 +34,7 @@ function timetable_form_display() {
 				</div>
 				<button type="submit" name="submit" class="btn btn-primary">Submit</button>
             </form>
+            <div class="col">
             <div class="row">
                 <div class="col" style="background-color: #99CCFF;">102</div>
                 <div class="col" style="background-color: #66CCFF;">Online</div>
@@ -45,6 +46,7 @@ function timetable_form_display() {
                 <div class="col" style="background-color: #FF99FF;">A210</div>
                 <div class="col" style="background-color: #D0FFD0;">A214</div>
                 <div class="col" style="background-color: #99DD99;">Online</div>
+            </div>
             </div>
         </div>
         <?php
