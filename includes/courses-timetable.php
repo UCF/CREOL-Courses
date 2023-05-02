0de7428c539ebc9fa0b505f2df5a41e46d7ccd2d
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-02 12:33:10 -0400

    test

--- includes/courses-timetable.php
+++ includes/courses-timetable.php
@@ -8,10 +8,9 @@ function timetable_form_display() {
     <div class="container">
         <div class="row">
             <form method="post" name="form">
-                <div class="col-3 form-group">
+                <div class="form-group">
                     <label for="semester">Semester</label>
 					<select name="semester" id="semester" class="form-control">
-						<option value=0>All</option>
 						<?php for ( $i = 0; $i < count( $semester_arr ); $i++ ) : ?>
 							<option value="<?= $semester_arr[ $i ]->SemesterSerial ?>" 
 							<?= ( isset($_POST['semester']) && $_POST['semester'] == $semester_arr[ $i ]->SemesterSerial ) ? 'selected=true' : '' ?>>
@@ -20,8 +19,7 @@ function timetable_form_display() {
 						<?php endfor; ?>
 					</select>
                 </div>
-                <div class="col-2">
-                    <div class="form-check">
+                <div class="form-check">
 						<label class="form-check-label">
 							<input id="undergrad" class="form-check-input" type="checkbox" name="undergrad" value=1 
 							<?= ( isset($_POST['undergrad'] ) &&  $_POST['undergrad'] == 1 ) ? 'checked' : '' ?>>
@@ -35,7 +33,6 @@ function timetable_form_display() {
 							 Graduate
 						</label>
 					</div>
-                </div>
                 <button type="submit" name="submit" class="btn btn-primary">Submit</button>
             </form>
             <div class="col-1">
@@ -67,8 +64,6 @@ function timetable_form_display() {
             <script>
 			    // Sets the form to the correct information.
 				document.getElementById("semester").selectedIndex = 1;
-				document.getElementById("instructor").selectedIndex = 0;
-			    document.getElementById("course").selectedIndex = 0;
 				document.getElementById("undergrad").checked = true;
 				document.getElementById("grad").checked = true;
 			</script>
