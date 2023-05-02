79b66cb224532c8267f2a047917736268f695723
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-02 12:39:25 -0400

    changed col spacing

--- includes/courses-timetable.php
+++ includes/courses-timetable.php
@@ -7,7 +7,7 @@ function timetable_form_display() {
     ?>
     <div class="container">
         <div class="row">
-            <form method="post" name="form" class="col">
+            <form method="post" name="form" class="col-3">
                 <div class="form-group">
                     <label for="semester">Semester</label>
 					<select name="semester" id="semester" class="form-control">
