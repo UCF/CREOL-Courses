21a8780125e401018c912e57a93899781dace70c
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-09 14:32:18 -0400

    test

--- timetable/timetable-class.php
+++ timetable/timetable-class.php
@@ -150,7 +150,11 @@ class TimeTable {
     
     private function get_time( $row ) {
         $time = ( $row + $this->start_time ) * 25;
-        if ( $time % 100 != 0 ) { $time = '&nbsp'; }
+        if ( $time % 100 != 0 ) {
+            $time = '&nbsp';
+        } else {
+            $time = substr( $time, 0, 1 ) * 3600;
+        }
         return $time;
     }
     
