58fb0afc26e620bb32c017d5ccd8928b810caaea
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-11 13:52:35 -0400

    put row into a container

--- timetable/timetable-shortcode.php
+++ timetable/timetable-shortcode.php
@@ -5,50 +5,52 @@ function timetable_form_display() {
 
 	ob_start();
 	?>
-	<div class="row">
-		<form method="post" name="form" id="timetableform" class="form-inline">
-			<div class="form-group m-2">
-				<select name="semester" id="semester" class="form-control" onchange="this.form.submit()">
-					<?php for ( $i = 0; $i < count( $semester_arr ); $i++ ) : ?>
-						<option value="<?= $semester_arr[ $i ]->SemesterSerial ?>" <?= ( isset( $_POST['semester'] ) && $_POST['semester'] == $semester_arr[ $i ]->SemesterSerial ) ? 'selected=true' : '' ?>>
-							<?= $semester_arr[ $i ]->SemesterTxt ?>
-						</option>
-					<?php endfor; ?>
-				</select>
+	<div class="container">
+		<div class="row">
+			<form method="post" name="form" id="timetableform" class="form-inline">
+				<div class="form-group m-2">
+					<select name="semester" id="semester" class="form-control" onchange="this.form.submit()">
+						<?php for ( $i = 0; $i < count( $semester_arr ); $i++ ) : ?>
+							<option value="<?= $semester_arr[ $i ]->SemesterSerial ?>" <?= ( isset( $_POST['semester'] ) && $_POST['semester'] == $semester_arr[ $i ]->SemesterSerial ) ? 'selected=true' : '' ?>>
+								<?= $semester_arr[ $i ]->SemesterTxt ?>
+							</option>
+						<?php endfor; ?>
+					</select>
+				</div>
+				<div class="form-check m-2">
+					<label class="form-check-label">
+						<input id="undergrad" class="form-check-input" type="checkbox" name="undergrad" value=1
+							onclick="this.form.submit()">
+						Undergraduate
+					</label>
+				</div>
+				<div class="form-check m-2">
+					<label class="form-check-label">
+						<input id="grad" class="form-check-input" type="checkbox" name="grad" value=0
+							onclick="this.form.submit()">
+						Graduate
+					</label>
+				</div>
+			</form>
+			<div class="col-auto m-2">
+				<a href="/courses/" class="btn btn-primary">Courses</a>
 			</div>
-			<div class="form-check m-2">
-				<label class="form-check-label">
-					<input id="undergrad" class="form-check-input" type="checkbox" name="undergrad" value=1
-						onclick="this.form.submit()">
-					Undergraduate
-				</label>
+			<div class="col-auto">
+				<hr class="hr-vertical">
 			</div>
-			<div class="form-check m-2">
-				<label class="form-check-label">
-					<input id="grad" class="form-check-input" type="checkbox" name="grad" value=0
-						onclick="this.form.submit()">
-					Graduate
-				</label>
-			</div>
-		</form>
-		<div class="col-auto m-2">
-			<a href="/courses/" class="btn btn-primary">Courses</a>
-		</div>
-		<div class="col-auto">
-			<hr class="hr-vertical">
-		</div>
-		<div class="col m-2">
-			<div class="row">
-				<div class="col" style="background-color: #99CCFF;">102</div>
-				<div class="col" style="background-color: #66CCFF;">Online</div>
-				<div class="col" style="background-color: #FFFF99;">103</div>
-				<div class="col" style="background-color: #C0C0C0;">265</div>
-				<div class="col" style="background-color: #FF9966;">266</div>
-				<div class="w-100"></div>
-				<div class="col" style="background-color: #CC6699;">A207</div>
-				<div class="col" style="background-color: #FF99FF;">A210</div>
-				<div class="col" style="background-color: #D0FFD0;">A214</div>
-				<div class="col" style="background-color: #99DD99;">Online</div>
+			<div class="col m-2">
+				<div class="row">
+					<div class="col" style="background-color: #99CCFF;">102</div>
+					<div class="col" style="background-color: #66CCFF;">Online</div>
+					<div class="col" style="background-color: #FFFF99;">103</div>
+					<div class="col" style="background-color: #C0C0C0;">265</div>
+					<div class="col" style="background-color: #FF9966;">266</div>
+					<div class="w-100"></div>
+					<div class="col" style="background-color: #CC6699;">A207</div>
+					<div class="col" style="background-color: #FF99FF;">A210</div>
+					<div class="col" style="background-color: #D0FFD0;">A214</div>
+					<div class="col" style="background-color: #99DD99;">Online</div>
+				</div>
 			</div>
 		</div>
 	</div>
