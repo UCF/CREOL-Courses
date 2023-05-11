19a58523e1082f98830ef2962ba30ee6e5e7076f
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-11 10:55:56 -0400

    removed back button

--- templates/details-template.php
+++ templates/details-template.php
@@ -7,33 +7,35 @@ function details_page( $course_id ) {
 	$syllabus_url = 'https://api.creol.ucf.edu/CoursesJson.asmx/SyllabusList?CourseID=' . $course_id;
 	$details_arr = get_json( $details_url );
 	$syllabus_arr = get_json( $syllabus_url );
-?>
-<div class="container">
-	<div class="mt-3 mt-sm-4 mt-md-5 mb-3">
-		<a href="/courses/" class="h2 text-primary"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i></a>
-		<h1><?= $details_arr[0]->Course . '- ' . $details_arr[0]->Title ?></h1>
-	</div>
-	<div class="row">
-		<div class="col">
-			<?= $details_arr[0]->Description ?>
-			<div class="mt-4 mb-5 pb-sm-4">
-				<?= $details_arr[0]->Details ?>
+	?>
+	<div class="container">
+		<div class="mt-3 mt-sm-4 mt-md-5 mb-3">
+			<h1>
+				<?= $details_arr[0]->Course . '- ' . $details_arr[0]->Title ?>
+			</h1>
+		</div>
+		<div class="row">
+			<div class="col">
+				<?= $details_arr[0]->Description ?>
+				<div class="mt-4 mb-5 pb-sm-4">
+					<?= $details_arr[0]->Details ?>
+				</div>
+			</div>
+			<div class="col-2">
+				<p class="font-weight-bold letter-spacing-1">Syllabi</p>
+				<ul class="list-group list-group-flush">
+					<?php foreach ( $syllabus_arr as $curr ) : ?>
+						<li class="list-group-item">
+							<a href="/courses/syllabus/?scheduleid=<?= $curr->CourseScheduleID ?>&course=<?= trim( $details_arr[0]->Course ) ?>"
+								target="_blank"><?= $curr->Semester ?></a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
 			</div>
 		</div>
-		<div class="col-2">
-			<p class="font-weight-bold letter-spacing-1">Syllabi</p>
-			<ul class="list-group list-group-flush">
-				<?php foreach ( $syllabus_arr as $curr ) : ?>
-				<li class="list-group-item">
-					<a href="/courses/syllabus/?scheduleid=<?= $curr->CourseScheduleID ?>&course=<?= trim( $details_arr[0]->Course ) ?>" target="_blank"><?= $curr->Semester ?></a>
-				</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
 	</div>
-</div>
 
-<?php
+	<?php
 }
 
 get_header();
@@ -44,11 +46,11 @@ the_post(); ?>
 	if ( isset( $_GET['courseid'] ) ) {
 		details_page( $_GET['courseid'] );
 	} else {
-	?>
-	<div class="container">
-		Go to the <a href="/courses/">Courses page</a> for details on a specific course.
-	</div>
-	<?php
+		?>
+		<div class="container">
+			Go to the <a href="/courses/">Courses page</a> for details on a specific course.
+		</div>
+		<?php
 	}
 	?>
 </article>
