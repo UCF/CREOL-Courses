31b63707585dd6ee35563feb21ed11f98c140566
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-18 10:56:57 -0400

    test

--- timetable/timetable-shortcode.php
+++ timetable/timetable-shortcode.php
@@ -22,28 +22,27 @@ function timetable_form_display() {
 						</select>
 					</div>
 			</form>
-			<div class="col-auto m-2">
-				<a href="/courses/" class="btn btn-primary">List View</a>
-			</div>
-			<div class="col-auto">
-				<hr class="hr-vertical">
-			</div>
-			<div class="col m-2">
-				<div class="row">
-					<div class="col" style="background-color: #99CCFF;">102</div>
-					<div class="col" style="background-color: #66CCFF;">Online</div>
-					<div class="col" style="background-color: #FFFF99;">103</div>
-					<div class="col" style="background-color: #C0C0C0;">265</div>
-					<div class="col" style="background-color: #FF9966;">266</div>
-					<div class="w-100"></div>
-					<div class="col" style="background-color: #CC6699;">A207</div>
-					<div class="col" style="background-color: #FF99FF;">A210</div>
-					<div class="col" style="background-color: #D0FFD0;">A214</div>
-					<div class="col" style="background-color: #99DD99;">Online</div>
-				</div>
+		</div>
+		<div class="col-auto m-2">
+			<a href="/courses/" class="btn btn-primary">List View</a>
+		</div>
+		<div class="col-auto">
+			<hr class="hr-vertical">
+		</div>
+		<div class="col m-2">
+			<div class="row">
+				<div class="col" style="background-color: #99CCFF;">102</div>
+				<div class="col" style="background-color: #66CCFF;">Online</div>
+				<div class="col" style="background-color: #FFFF99;">103</div>
+				<div class="col" style="background-color: #C0C0C0;">265</div>
+				<div class="col" style="background-color: #FF9966;">266</div>
+				<div class="w-100"></div>
+				<div class="col" style="background-color: #CC6699;">A207</div>
+				<div class="col" style="background-color: #FF99FF;">A210</div>
+				<div class="col" style="background-color: #D0FFD0;">A214</div>
+				<div class="col" style="background-color: #99DD99;">Online</div>
 			</div>
 		</div>
-	</div>
 	</div>
 	<?php
 	ob_start();
