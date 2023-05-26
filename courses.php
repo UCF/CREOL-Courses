29344039113ebe78f3a62fbb55919c7eb0ed65ee
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-05-26 14:22:37 -0400

    added shortcode for list of all courses

--- courses.php
+++ courses.php
@@ -16,15 +16,19 @@ define( 'ALL_SEMESTERS', 0 );
 define( 'ALL_INSTRUCTORS', -1 );
 define( 'ALL_COURSES', 0 );
 define( 'UNDERGRAD_GRAD', 2 );
+define( 'UNDERGRAD', 1 );
+define( 'GRAD', 0 );
 
 require_once 'includes/courses-feed.php';
 require_once 'includes/courses-layout.php';
 require_once 'includes/courses-functions.php';
+require_once 'includes/courses-list.php';
 require_once 'timetable/timetable-shortcode.php';
 require_once 'timetable/timetable-class.php';
 require_once 'templates/template-functions.php';
 
-add_shortcode( 'courses', 'courses_form_display' );
+add_shortcode( 'courses', 'courses_list' );
+add_shortcode( 'course-schedule', 'courses_form_display' );
 add_shortcode( 'timetable', 'timetable_form_display' );
 add_filter( 'theme_page_templates', 'add_page_template' );
 add_filter( 'template_include', 'change_page_template', 99 );
---
+++ includes/courses-list.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 
+ **/
+
+function courses_list() {
+	$undergrad_arr = get_json( 'https://api.creol.ucf.edu/CoursesJson.asmx/UndergradCourses' );
+	$grad_arr = get_json( 'https://api.creol.ucf.edu/CoursesJson.asmx/GradCourses' );
+
+	ob_start();
+	?>
+	<div style="padding: 5% 10% 5% 10%">
+		<h2 id="undergrad" class="mb-0 auto-section" data-section-link-title="Undergraduate">Undergraduate Courses</h2>
+		<hr class=" hr-2 hr-black my-2">
+		<?php courses_list_display( UNDERGRAD ); ?>
+	</div>
+	<div style="padding: 5% 10% 5% 10%">
+		<h2 id="grad" class="mb-0 auto-section" data-section-link-title="Graduate">Graduate Courses</h2>
+		<hr class=" hr-2 hr-black my-2">
+		<?php courses_list_display( GRAD ); ?>
+	</div>
+	<?php
+	return ob_get_clean();
+}
+
+function courses_list_display( $level ) {
+	$url = ( $level == UNDERGRAD ) ? 'https://api.creol.ucf.edu/CoursesJson.asmx/UndergradCourses' : 'https://api.creol.ucf.edu/CoursesJson.asmx/GradCourses';
+	$courses = get_json( $url );
+
+	foreach ( $courses as $curr ) {
+		?>
+		<div class="px-2 pb-3">
+			<span class="h-5 font-weight-bold letter-spacing-1">
+				<?= $curr->Course . ' ' . $curr->Title ?>
+			</span><br>
+			<?php if ( $curr->Description != null ) : ?>
+				<span>
+					<?= $curr->Description ?>
+				</span><br>
+			<?php endif; ?>
+			|
+			<?= $curr->isDetail ? ( '<a href="details/?courseid=' . $curr->CourseID . '">Details</a> | ' ) : '' ?>
+			<a href="courses/?semester=0&instructor=-1&course=<?= $curr->CourseID ?>&level=<?= $level ?>">Schedule</a>
+			|
+			<a href="https://creol.ucf.edu/academics/undergrad/mission-educational-objectives-abet/abet-prep/">ABET</a>
+			|
+			<a href="syllabus/?scheduleid=<?= $curr->CourseScheduleID ?>&course=<?= $curr->Course ?>">Syllabus</a> |
+		</div>
+		<?php
+	}
+}
