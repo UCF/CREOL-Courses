fa8c376da26e5f9584795d2f99fd90be0cc72f38
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-10-09 07:29:30 -0700

    removing cache for testing

--- includes/courses-feed.php
+++ includes/courses-feed.php
@@ -4,20 +4,23 @@
  * Takes in a URL and returns an object.
  **/
 function get_json( $url ) {
-	$transient = 'courses_' . md5( $url );
-	$items = get_transient( $transient );
-	$expiration = 60; // Seconds in an hour.
+	// $transient = 'courses_' . md5( $url );
+	// $items = get_transient( $transient );
+	// $expiration = 60; // Seconds in an hour.
 
-	if ( ! $items ) {
-		$request = wp_remote_get( $url );
+	// if ( ! $items ) {
+	// 	$request = wp_remote_get( $url );
 
-		if ( is_wp_error( $request ) ) {
-			return false;
-		}
+	// 	if ( is_wp_error( $request ) ) {
+	// 		return false;
+	// 	}
 
-		$items = json_decode( wp_remote_retrieve_body( $request ) );
-		set_transient( $transient, $items, $expiration );
-	}
+	// 	$items = json_decode( wp_remote_retrieve_body( $request ) );
+	// 	set_transient( $transient, $items, $expiration );
+	// }
+
+	$request = wp_remote_get( $url );
+	$items = json_decode( wp_remote_retrieve_body( $request ) );
 
 	$items = array( $items->response )[0];
 
