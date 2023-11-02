781ff0f67f32acc4b6dd549b328811b8343c1bcb
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-11-02 11:01:25 -0400

    test

--- includes/courses-feed.php
+++ includes/courses-feed.php
@@ -7,11 +7,16 @@ function get_json( $url ) {
 	$transient = 'courses_' . md5( $url );
 	$items = get_transient( $transient );
 	$expiration = 3600; // Seconds in an hour.
+	$args = array(
+		'timeout' => 60,
+	);
 
 	if ( ! $items ) {
-		$request = wp_remote_get( $url );
+		$request = wp_remote_get( $url, $args );
 
 		if ( is_wp_error( $request ) ) {
+			echo 'Please email UCFTeam-CREOL-IT@groups.ucf.edu with the url, error message, and screenshot.\n';
+			echo $request->get_error_message() . '\n';
 			return false;
 		}
 
