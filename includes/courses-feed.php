0231ca8149c83fd701197a8c8b2529f0e0be2ac0
Claire Daugherty <98245504+claire-md@users.noreply.github.com>
2023-11-02 11:00:48 -0400

    test

--- includes/courses-feed.php
+++ includes/courses-feed.php
@@ -3,41 +3,23 @@
  * Handles JSON feed.
  * Takes in a URL and returns an object.
  **/
-// function get_json( $url ) {
-// 	$transient = 'courses_' . md5( $url );
-// 	$items = get_transient( $transient );
-// 	$expiration = 3600; // Seconds in an hour.
-
-// 	if ( ! $items ) {
-// 		$request = wp_remote_get( $url );
-
-// 		if ( is_wp_error( $request ) ) {
-// 			return false;
-// 		}
-
-// 		$items = json_decode( wp_remote_retrieve_body( $request ) );
-// 		set_transient( $transient, $items, $expiration );
-// 	}
-
-// 	$items = json_decode( wp_remote_retrieve_body( $request ) );
-
-// 	$items = array( $items->response )[0];
-
-// 	return $items;
-// }
-
 function get_json( $url ) {
-	$args = array(
-		'timeout' => 60,	
-	);
-	$request = wp_remote_get( $url, $args );
+	$transient = 'courses_' . md5( $url );
+	$items = get_transient( $transient );
+	$expiration = 3600; // Seconds in an hour.
 
-	if ( is_wp_error( $request ) ) {
-		echo $request->get_error_message();
-		return false;
+	if ( ! $items ) {
+		$request = wp_remote_get( $url );
+
+		if ( is_wp_error( $request ) ) {
+			return false;
+		}
+
+		$items = json_decode( wp_remote_retrieve_body( $request ) );
+		set_transient( $transient, $items, $expiration );
 	}
 
-	$items = json_decode( wp_remote_retrieve_body( $request ) );
+	// $items = json_decode( wp_remote_retrieve_body( $request ) );
 
 	$items = array( $items->response )[0];
 
