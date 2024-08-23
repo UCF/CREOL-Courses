93492f461de3f99403f175b1d42c4ba4b27ed416
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-08-23 21:40:42 +0000

    ...

--- includes/courses-feed.php
+++ includes/courses-feed.php
@@ -6,6 +6,7 @@
 function get_json( $url ) {
 	$transient = 'courses_' . md5( $url );
 	$items = get_transient( $transient );
+	// Do NOT set expiration to zero. This results in a cache that will never be updated.
 	$expiration = 60; // Seconds in an hour.
 	$args = array(
 		'timeout' => 60,
